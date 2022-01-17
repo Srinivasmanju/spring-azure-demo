6688f76ff60a07770d2297ed68b11ef65654c465
srinivas_manjunath <srinivasmanju8123@gmail.com>
2022-01-17 12:11:19 +0530

    Second

--- src/main/java/com/azurelearn/springazuredemo/SpringAzureDemoApplication.java
+++ src/main/java/com/azurelearn/springazuredemo/SpringAzureDemoApplication.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class SpringAzureDemoApplication {
 	@GetMapping("/message")
 	public String message(){
-		return "Welcome";
+		return "Welcome to azure platform";
 	}
 
 	public static void main(String[] args) {
